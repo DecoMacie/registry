03f8bce3a1431ede9c744ffef9ea26e0a5752505
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2025-07-04 00:39:59 +0100

    Connecting Page and Loader
    Create an interface to connect SearchLoader.ts and SearchPage.tsx

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -1,7 +1,8 @@
+import type { SearchLoaderResult } from "./searchLoader";
 import { useLoaderData } from "react-router-dom";
 
 export default function SearchPage() {
-  const data = useLoaderData();
+  const data = useLoaderData() as SearchLoaderResult;
   console.log(data);
   return <div>Search page</div>;
 }
--- src/pages/Search/searchLoader.ts
+++ src/pages/Search/searchLoader.ts
@@ -1,13 +1,18 @@
+import type { packageSummary } from "../../api/types/packageSummary";
 import { searchPackages } from "../../api/queries/searchPackages";
 
-export async function searchLoader({ request}: {request: Request}) {
+export interface SearchLoaderResult {
+    searchResults: packageSummary[];
+}
+
+export async function searchLoader({ request}: {request: Request}): Promise<SearchLoaderResult> {
     const { searchParams } = new URL(request.url);
     const term = searchParams.get("term");
     if (!term) {
         throw new Error("Search term must be provided");
     }
 
-    const results = searchPackages(term);
+    const results = await searchPackages(term);
 
     return {
         searchResults: results
