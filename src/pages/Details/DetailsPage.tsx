4db8cb8bd8b4ce07c5879e45862c953698d309f2
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2025-07-28 18:49:15 +0100

    Added Printscreen

--- src/pages/Details/DetailsPage.tsx
+++ src/pages/Details/DetailsPage.tsx
@@ -7,15 +7,15 @@ export default function DetailsPage() {
     <div className="space-y-4">
       <h1 className="text-3xl font-bold">{details.name}</h1>
       <div>
-        <h3 className="text-lg font-bold">Description</h3>
+        <h2 className="text-lg font-semibold">Description</h2>
         <div className="p-3 bg-gray-200 rounded">{details.description}</div>
       </div>
       <div>
-        <h3 className="text-lg font-bold">Licence</h3>
+        <h2 className="text-lg font-semibold">Licence</h2>
         <div className="p-3 bg-gray-200 rounded">{details.license}</div>
       </div>
       <div>
-        <h3 className="text-lg font-bold">Author</h3>
+        <h2 className="text-lg font-semibold">Author</h2>
         <div className="p-3 bg-gray-200 rounded">{details.author?.name}</div>
       </div>
     </div>
